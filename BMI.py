e4f84141927cf4214d5e4804a5cb6b8198524777
PearlRowe <146419328+PearlRowe@users.noreply.github.com>
2023-10-06 16:47:53 +0300

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,2 +1,10 @@
 height=float(input ("enter your height"))
 weight=float(input("enter your weight"))
+BMI=weight/(height)*2
+print("Your BMI is :" ,BMI)
+if BMI <=18.5:
+  print("Underweight")
+elif BMI <=24:
+  print ("normal")
+  else BMI <=29.9:
+  print("overweight")
